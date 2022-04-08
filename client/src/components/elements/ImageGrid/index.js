e1770ec2e7943d06d523fc8c4614cf097bcf3421
David G <davidgoe85@gmail.com>
2022-04-08 19:57:02 +0200

    Added structure and functionality for home

---
+++ client/src/components/elements/ImageGrid/index.js
@@ -0,0 +1,24 @@
+import { address } from "../../../assets/data";
+
+function ImageGrid({ currentGridData, address }) {
+  return (
+    <section className="card-setup image-grid py-6 justify-center">
+      {currentGridData.map((item, index) => {
+        return (
+          <div
+            key={index}
+            className="col-span-12 lg:col-span-6 xl:col-span-4 overflow-hidden"
+          >
+            <img
+              className=" "
+              src={`${address.url}${item.photo}`}
+              alt="gridimage"
+            />
+          </div>
+        );
+      })}
+    </section>
+  );
+}
+
+export default ImageGrid;
