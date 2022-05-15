d7840d7e2b1c10180a50ed6abf72c071b12f6aca
DaGo85 <davidgoe85@gmail.com>
2022-05-15 18:45:21 +0200

    prepare for deploy

--- server/config/key.js
+++
@@ -1,6 +0,0 @@
-// Divide dev (local) and prod (distribution) environments
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./prod");
-} else {
-  module.exports = require("./dev");
-}
--- server/config/prod.js
+++
@@ -1,6 +0,0 @@
-// When running in a deployment environment (AWS, Heroku...)
-module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  emailAddress: process.env.EMAIL_ADDRESS,
-  emailPassword: process.env.EMAIL_PASSWORD,
-};
