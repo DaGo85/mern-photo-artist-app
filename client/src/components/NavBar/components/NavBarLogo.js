74d473ff7d3a3c62e352d025c46f766d29b66e02
DaGo85 <davidgoe85@gmail.com>
2022-05-24 12:35:56 +0200

    added new logo redirects etc

--- client/src/components/NavBar/components/NavBarLogo.js
+++ client/src/components/NavBar/components/NavBarLogo.js
@@ -1,6 +1,6 @@
 // Logo component for NavBar
 
-import ago5 from "../../../assets/images/ago7.png";
+import ago from "../../../assets/images/ago7.png";
 
 import { Link } from "react-router-dom";
 
@@ -9,7 +9,7 @@ function NavBarLogo() {
     <Link to="/">
       <img
         className="max-h-20 min-w-[12] navsvghover"
-        src={ago5}
+        src={ago}
         alt="Logo des Betreibers der Seite"
       />
     </Link>
