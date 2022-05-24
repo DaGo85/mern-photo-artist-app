6e82d0c30673bd8d469728de0153c04d7fa19ad2
DaGo85 <davidgoe85@gmail.com>
2022-05-24 11:48:25 +0200

    test for git

--- client/src/components/NavBar/components/NavBarLogo.js
+++ client/src/components/NavBar/components/NavBarLogo.js
@@ -1,6 +1,6 @@
 // Logo component for NavBar
 
-import ago from "../../../assets/images/ago-logo.png";
+import ago5 from "../../../assets/images/ago7.png";
 
 import { Link } from "react-router-dom";
 
@@ -9,7 +9,7 @@ function NavBarLogo() {
     <Link to="/">
       <img
         className="max-h-20 min-w-[12] navsvghover"
-        src={ago}
+        src={ago5}
         alt="Logo des Betreibers der Seite"
       />
     </Link>
