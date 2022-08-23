6581943f20912b565d93d16fcbcb27326e07eac6
DaGo85 <davidgoe85@gmail.com>
2022-08-23 19:16:00 +0200

    changed env and personal data

--- client/src/pages/Impressum/Impressum.js
+++ client/src/pages/Impressum/Impressum.js
@@ -16,75 +16,57 @@ function Impressum() {
   return (
     <TransitionWrapper>
       <main>
-        <div className={`bg-setup ${bg}`}>
-          <PageHeadLine headline={"Impressum"} />
-          <SubText subtext={subtexts.impressum} />
+        <div className={`bg-setup ${bg}`}></div>
+        <PageHeadLine headline={"Impressum"} />
+        <SubText subtext={subtexts.impressum} />
 
-          <section className="card-setup gap-6 pt-6 pb-40">
-            <h4>AGO Photography</h4>
-            <span className="flex flex-col gap-4 w-full px-6">
-              <h5>Gemäß zu § 5 TMG:</h5>
-              <hr />
-              <address>
-                Achim und Anne Goergens
-                <br />
-                Dorstener Strasse 534
-                <br />
-                46119 Oberhausen
-              </address>
-            </span>
-            <span className="flex flex-col gap-4 w-full px-6">
-              <h5>Kontakt:</h5>
-              <hr />
-              <address>
-                E-Mail: goergensjoa@gmail.com <br />
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  className="underline navsvghover"
-                  href="https://www.facebook.com/an.na.988711/"
-                >
-                  Annes Facebook Profil
-                </a>
-                <br />
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  className="underline navsvghover"
-                  href="https://www.facebook.com/achim.goergens"
-                >
-                  Achims Facebook Profil
-                </a>
-              </address>
-              <hr />
-            </span>
-            <span className="flex flex-col gap-4 w-full px-6">
-              <h5>Haftungsausschluss</h5>
-              <hr />
-              <p>
-                Für den Inhalt der Webseite: www.ago-photography.com sind Anne
-                und Achim Goergens verantwortlich. Die Webseite wird rein privat
-                betrieben und hat keinerlei kommerzielle Interessen.
-                <br />
-                Die auf dieser Webseite verwendeten Bilder unterliegen dem
-                Copyright. Ohne eine vorherige Anfrage und dadurch
-                gegebenenfalls erteilte Erlaubnis, dürfen diese Bilder nicht
-                verwendet, heruntergeladen oder verbreitet werden.
-                <br />
-                Für den Inhalt dieser Seite sind die Autoren zuständig. Es kann
-                keine Garantie für die Richtigkeit der angegebenen Informationen
-                gegeben werden. Eine dauerhafte technische Erreichbarkeit ist
-                nicht vorhanden. Dazu besteht ein Haftungsausschluss für den
-                Inhalt von verlinkten Webseiten, auf deren Inhalt kein Einfluss
-                von unserer Seite aus genommen werden kann. Gegebenenfalls lässt
-                sich der Inhalt dieser Webseite durch eine Anfrage mithilfe
-                eines Hinweises auf die entsprechende Stelle ändern.
-              </p>
-            </span>
-            <ImpressumSocialIcons />
-          </section>
-          <RandomImage />
-        </div>
+        <section className="gap-6 pt-6 pb-40 card-setup">
+          <h4>AGO Photography</h4>
+          <span className="flex flex-col w-full gap-4 px-6">
+            <h5>Gemäß zu § 5 TMG:</h5>
+            <hr />
+            <address>
+              David Goergens
+              <br />
+              Lutertalstrasse 25
+              <br />
+              3065 Bern
+            </address>
+          </span>
+          <span className="flex flex-col w-full gap-4 px-6">
+            <h5>Kontakt:</h5>
+            <hr />
+            <address>
+              E-Mail: davegoergens@gmail.com <br />
+            </address>
+            <hr />
+          </span>
+          <span className="flex flex-col w-full gap-4 px-6">
+            <h5>Haftungsausschluss</h5>
+            <hr />
+            <p>
+              Für den Inhalt der Webseite: www.ago-photography.com sind Anne und
+              Achim Goergens verantwortlich. Die Webseite wird rein privat
+              betrieben und hat keinerlei kommerzielle Interessen.
+              <br />
+              Die auf dieser Webseite verwendeten Bilder unterliegen dem
+              Copyright. Ohne eine vorherige Anfrage und dadurch gegebenenfalls
+              erteilte Erlaubnis, dürfen diese Bilder nicht verwendet,
+              heruntergeladen oder verbreitet werden.
+              <br />
+              Für den Inhalt dieser Seite sind die Autoren zuständig. Es kann
+              keine Garantie für die Richtigkeit der angegebenen Informationen
+              gegeben werden. Eine dauerhafte technische Erreichbarkeit ist
+              nicht vorhanden. Dazu besteht ein Haftungsausschluss für den
+              Inhalt von verlinkten Webseiten, auf deren Inhalt kein Einfluss
+              von unserer Seite aus genommen werden kann. Gegebenenfalls lässt
+              sich der Inhalt dieser Webseite durch eine Anfrage mithilfe eines
+              Hinweises auf die entsprechende Stelle ändern.
+            </p>
+          </span>
+          <ImpressumSocialIcons />
+        </section>
+        <RandomImage />
       </main>
     </TransitionWrapper>
   );
